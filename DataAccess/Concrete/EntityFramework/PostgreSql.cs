9328a9c14ce53fa73f4ddd037b8fac6f5bf0ac7a
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-10-01 11:56:52 +0300

    DataAccess files created

---
+++ DataAccess/Concrete/EntityFramework/PostgreSql.cs
@@ -0,0 +1,18 @@
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework;
+
+public class PostgreSqlContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Port=5432;Username=postgres;Password=mysecretpassword;Database=postgres");
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+}
