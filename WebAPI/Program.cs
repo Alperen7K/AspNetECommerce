a5e3b758ffe4d1550dead26298999315e746a7c4
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-10-02 19:32:35 +0300

    something changed

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,15 +1,34 @@
+using System.Text;
+using Core.Utilities.Security.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using Core.Utilities.Security.Encyption;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var tokenOptions = new TokenOptions();
+builder.Configuration.GetSection("TokenOptions").Bind(tokenOptions);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = tokenOptions.Issuer,
+        ValidAudience = tokenOptions.Audience,
+        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey),
+    };
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
