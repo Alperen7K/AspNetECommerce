08aaa61c7c2156821a1d8798e664d2ada1a6b1cc
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-10-04 10:28:16 +0300

    .

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -5,16 +5,17 @@ using Core.Entities;
 
 namespace Core.Entities.Concrete
 {
-    public class User:IEntity
+    public class User : IEntity
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+
         public byte[] PasswordSalt { get; set; }
+
         public byte[] PasswordHash { get; set; }
+
         public bool Status { get; set; }
-
-
     }
 }
